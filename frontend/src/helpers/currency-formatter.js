a32b0d1b377b15724520bd1d5925926c6c7b3f2f
dave <dave792@github.com>
2021-03-08 13:47:09 -0800

    First commit

--- frontend/src/helpers/currency-formatter.js
+++
@@ -1,9 +0,0 @@
-export const formatPrice = (price, currency = "USD") => {
-  const formatter = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: currency,
-    minimumFractionDigits: 2,
-  })
-
-  return formatter.format(price)
-}
